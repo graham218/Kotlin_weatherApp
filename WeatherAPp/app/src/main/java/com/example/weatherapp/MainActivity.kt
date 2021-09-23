57b7156c62dfd11f487b40a5ada86f6b0e2bf29e
BILL GRAHAM <grahambill011@gmail.com>
2021-09-23 04:51:35 +0300

    final touches

--- WeatherAPp/app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ WeatherAPp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -52,20 +52,43 @@ class MainActivity : AppCompatActivity() {
 
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
-            val jsonObj=JSONObject(result)
-            val main=jsonObj.getJSONObject("main")
-            val sys=jsonObj.getJSONObject("sys")
-            val wind=jsonObj.getJSONObject("wind")
-            val weather=jsonObj.getJSONArray("weather").getJSONObject(0)
-            val updatedAt:Long=jsonObj.getLong("dt")
-            val updatedAtText="Updated at: "+SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
-            val temp=main.getString("temp")+"*C"
-            val tempMin="Min Temp: "+main.getString("temp_min")+"*C"
-            val tempMax="Max Temp: "+main.getString("temp_max")+"*C"
-            val pressure=main.getString("pressure")
-            val humidity=main.getString("humidity")
-            val sunrise=main.getString("sunrise")
-            val sunset=main.getString("sunset")
+            try {
+                val jsonObj=JSONObject(result)
+                val main=jsonObj.getJSONObject("main")
+                val sys=jsonObj.getJSONObject("sys")
+                val wind=jsonObj.getJSONObject("wind")
+                val weather=jsonObj.getJSONArray("weather").getJSONObject(0)
+                val updatedAt:Long=jsonObj.getLong("dt")
+                val updatedAtText="Updated at: "+SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
+                val temp=main.getString("temp")+"*C"
+                val tempMin="Min Temp: "+main.getString("temp_min")+"*C"
+                val tempMax="Max Temp: "+main.getString("temp_max")+"*C"
+                val pressure=main.getString("pressure")
+                val humidity=main.getString("humidity")
+                val sunrise:Long=sys.getLong("sunrise")
+                val sunset:Long=sys.getLong("sunset")
+                val windSpeed=wind.getString("speed")
+                val weatherDescription=weather.getString("description")
+                val address=jsonObj.getString("name")+","+sys.getString("country")
+
+                findViewById<TextView>(R.id.adress).text=address
+                findViewById<TextView>(R.id.updated_at).text=updatedAtText
+                findViewById<TextView>(R.id.status).text=weatherDescription.capitalize()
+                findViewById<TextView>(R.id.temp).text=temp
+                findViewById<TextView>(R.id.temp_min).text=tempMin
+                findViewById<TextView>(R.id.temp_max).text=tempMax
+                findViewById<TextView>(R.id.sunrise).text=SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
+                findViewById<TextView>(R.id.sunset).text=SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
+                findViewById<TextView>(R.id.wind).text=windSpeed
+                findViewById<TextView>(R.id.pressure).text=pressure
+                findViewById<TextView>(R.id.humidity).text=humidity
+
+                findViewById<ProgressBar>(R.id.loader).visibility=View.GONE
+                findViewById<RelativeLayout>(R.id.mainContainer).visibility=View.VISIBLE
+            }catch (e: Exception){
+                findViewById<ProgressBar>(R.id.loader).visibility=View.GONE
+                findViewById<TextView>(R.id.errortext).visibility=View.VISIBLE
+            }
         }
     }
 }
