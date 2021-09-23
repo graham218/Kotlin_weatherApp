0e32aa47aa60fbc287137a126dd5a985f21d93d6
BILL GRAHAM <grahambill011@gmail.com>
2021-09-23 03:40:24 +0300

    Update MainActivity.kt

--- WeatherAPp/app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ WeatherAPp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -15,8 +15,11 @@ import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import android.widget.TextView
 import com.example.weatherapp.databinding.ActivityMainBinding
+import org.json.JSONObject
 import java.lang.Exception
 import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -45,6 +48,17 @@ class MainActivity : AppCompatActivity() {
                 response=null
             }
             return response
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            val jsonObj=JSONObject(result)
+            val main=jsonObj.getJSONObject("main")
+            val sys=jsonObj.getJSONObject("sys")
+            val wind=jsonObj.getJSONObject("wind")
+            val weather=jsonObj.getJSONArray("weather").getJSONObject(0)
+            val updatedAt:Long=jsonObj.getLong("dt")
+            val updatedAtText="Updated at: "+SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
         }
     }
 }
