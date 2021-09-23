96ff3219120f4244f9840982411d04b1f9ac99e5
BILL GRAHAM <grahambill011@gmail.com>
2021-09-23 03:26:40 +0300

    update main activity

--- WeatherAPp/app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ WeatherAPp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.weatherapp
 
+import android.os.AsyncTask
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,7 +10,13 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.TextView
 import com.example.weatherapp.databinding.ActivityMainBinding
+import java.lang.Exception
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,5 +26,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        weatherTask().execute()
+    }
+    inner class weatherTask(): AsyncTask<String, Void, String>(){
+        override fun onPreExecute() {
+            super.onPreExecute()
+            findViewById<ProgressBar>(R.id.loader).visibility= View.VISIBLE
+            findViewById<RelativeLayout>(R.id.mainContainer).visibility= View.GONE
+            findViewById<TextView>(R.id.errortext).visibility= View.GONE
+        }
+        override fun doInBackground(vararg p0: String?): String? {
+            var response:String?
+            try {
+                response= URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API").readText(Charsets.UTF_8)
+
+            }
+            catch (e: Exception){
+                response=null
+            }
+            return response
+        }
     }
 }
